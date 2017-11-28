b73d355b4b3dd2e87752c0f78053382421bf21ec
andersonericw <anderson.ericw@gmail.com>
2017-11-28 12:48:44 -0500

    Update threading.rb

--- threading.rb
+++ threading.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/ruby
 
+#Do a single threaded run, timing each iteration of the array [0,1], doing two iterations because of two elements
 before = Time.now
 [0,1].each do |i|
   sleep 1
 end
-Process.waitall
 after = Time.now
 delta = (after - before) * 1000.0
-puts "Single-threaded: #{delta} milliseconds"
+puts "Single-threaded: #{delta} milliseconds" #Should be 1000 miliseconds X number of elements in the array (2)
 
+#Do a multi threaded run, timing each iterationg of the array [0,1], doing two iterations because of the two elements
 before = Time.now
 [0,1].each do |i|
-  fork do
+  fork do #Instead of running each operation sequentially, run each process in a different thread
     sleep 1
   end
 
@@ -19,4 +20,4 @@ end
 Process.waitall
 after = Time.now
 delta = (after - before) * 1000.0
-puts "Multi-threaded: #{delta} milliseconds"
+puts "Multi-threaded: #{delta} milliseconds" #Should be 1000 miliseconds X 1 while array length < CPU Core count
